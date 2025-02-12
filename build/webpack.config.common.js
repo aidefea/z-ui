387e76cb0ec856d2efdddf8d5624c27013f12fa6
zyg <3496912013@qq.com>
2025-02-12 17:30:26 +0800

    修改配置

--- build/webpack.config.common.js
+++ build/webpack.config.common.js
@@ -2,7 +2,7 @@
 const path = require("path");
 const { VueLoaderPlugin } = require("vue-loader");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const isDev = process.env.NODE_ENV === "development";
 
