ae86159731b6d020aa7bd807b362a5930378f45c
zyg <3496912013@qq.com>
2025-02-15 03:41:01 +0800

    修改组件的引入方式

--- docs/.vuepress/.temp/internal/themeData.js
+++ docs/.vuepress/.temp/internal/themeData.js
@@ -1,1 +1,14 @@
 export const themeData = JSON.parse("{\"logo\":\"../assets/img/ng.png\",\"navbar\":[{\"text\":\"Z-ZYGUI\",\"link\":\"/\"},{\"text\":\"组件\",\"link\":\"/componentmd/\"}],\"sidebar\":{\"/\":[{\"text\":\"入门指南\",\"children\":[\"/\"]}],\"/componentmd/\":[{\"text\":\"组件库\",\"children\":[\"/componentmd/button\",\"/componentmd/toast\",\"/componentmd/popup.md\"]}]},\"plugins\":[{\"name\":\"@vuepress/plugin-register-components\",\"multiple\":true}],\"footer\":\"Made with VuePress\",\"repo\":\"https://gitee.com/CQSDJXCXZ/util\",\"search\":true,\"home\":\"/\",\"heroText\":\"My Awesome Blog\",\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"colorMode\":\"auto\",\"colorModeSwitch\":true,\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebarDepth\":2,\"editLink\":true,\"editLinkText\":\"Edit this page\",\"lastUpdated\":true,\"lastUpdatedText\":\"Last Updated\",\"contributors\":true,\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateThemeData) {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ themeData }) => {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  })
+}
--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,3 +1,16 @@
 import comp from "D:/Code/util/docs/.vuepress/.temp/pages/index.html.vue"
 const data = JSON.parse("{\"path\":\"/\",\"title\":\"项目名称\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"项目简介\",\"slug\":\"项目简介\",\"link\":\"#项目简介\",\"children\":[]},{\"level\":2,\"title\":\"安装与使用\",\"slug\":\"安装与使用\",\"link\":\"#安装与使用\",\"children\":[{\"level\":3,\"title\":\"安装\",\"slug\":\"安装\",\"link\":\"#安装\",\"children\":[]}]}],\"git\":{\"updatedTime\":1739558690000,\"contributors\":[{\"name\":\"zyg\",\"username\":\"zyg\",\"email\":\"3496912013@qq.com\",\"commits\":3,\"url\":\"https://github.com/zyg\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
